b83c6b805f56b2f18a3f710905592774089149a3
senaybee <senay_berihu@outlook.com>
2020-06-20 17:11:30 -0700

    Handle form input for answer to the riddle

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const FirstPage = (props) => {
-    return (
-        <div>
-            <p>The first page awaits your answer. As soon as you know, enter it in the form below</p>
-            <input type='text'/>
-            <Link to='/'>
-                <p className='bottom-right'>To the home page</p>
-            </Link>
-        </div>
-    );
+class FirstPage extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            text : '',
+            answer: 'david',
+            isRight : false,
+            isAnswered: false
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            text : event.target.value.toLowerCase()
+        })
+    }
+
+    handleKeyDown(event){
+        if(event.keyCode === 13){
+            if(this.state.text === this.state.answer){
+                this.setState({
+                    isAnswered: true,
+                    isRight: true
+                });
+            }
+        }
+    }
+    render() {
+        return (
+            <div>
+                <p>The first page awaits your answer. As soon as you know, enter it in the form below</p>
+                <input type='text' value={this.state.text} onChange={this.handleChange} onKeyDown={this.handleKeyDown}/>
+                <p style={{display: this.state.isAnswered && this.state.isRight ? "block" : "none"}}>You have solved The Riddle!</p>
+                <Link to='/'>
+                    <p className='bottom-right'>To the home page</p>
+                </Link>
+            </div>
+        );
+    }
 }
 
 export default FirstPage;
